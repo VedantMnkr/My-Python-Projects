cd8e7f8ab7119b67946bf9aeef955d04098b4e26
Vedant P. Mankar <82722096+VedantMnkr@users.noreply.github.com>
2023-08-28 13:36:19 +0530

    Add files via upload

---
+++ flappy_bird/fb_settings.py
@@ -0,0 +1,10 @@
+# Screen Resolution
+screen_width = 864
+screen_height =756
+
+#define colours
+white = (255, 255, 255)
+
+# Game entites
+pipe_gap = 200
+pipe_frequency = 1000  #milliseconds // frequency at which the pipe occurs
