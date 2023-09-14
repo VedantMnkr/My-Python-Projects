1097dd75685642de146e46b3a257fe73cd344b1c
Vedant P. Mankar <82722096+VedantMnkr@users.noreply.github.com>
2023-09-14 13:38:05 +0530

    Update fb_settings.py
    added new required variables and constants

--- flappy_bird/fb_settings.py
+++ flappy_bird/fb_settings.py
@@ -1,10 +1,32 @@
+from screeninfo import get_monitors
+for m in get_monitors():
+    x = m
+
+systemResHeight = x.height
+systemResWidth = x.width
+
 # Screen Resolution
-screen_width = 864
-screen_height =756
+# screen_width = int(8 * 110)
+# screen_height = int(7 * 110)
+
+screen_width = 960
+screen_height = 1020
 
 #define colours
 white = (255, 255, 255)
 
 # Game entites
-pipe_gap = 200
-pipe_frequency = 1000  #milliseconds // frequency at which the pipe occurs
+precision = 20
+pipe_gap = 40 + (precision * 4) # 40 -> bird height + (precision * 4 constant)
+scroll_speed = 4
+pipe_frequency = 8000 / scroll_speed #milliseconds // frequency at which the pipe occurs
+speed = 10
+pipeHeightIncrement = speed * 4 * 2
+
+# angle = 90
+angleup  = 90
+angledown = 90
+pipe_height, flag = 0, 1 # 1 means its going up and 0 means downwards
+pipeUpperLimit = angledown *4  #this goes down
+pipeLowerLimit = -(angleup * 4) #this goes up
+pipeHeightPiviot = -pipe_height
