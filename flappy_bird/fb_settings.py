f6004dacd4f37969fce11608e01b9dc3df6e3e3d
Vedant P. Mankar <82722096+VedantMnkr@users.noreply.github.com>
2023-09-25 16:03:35 +0530

    Update fb_settings.py

--- flappy_bird/fb_settings.py
+++ flappy_bird/fb_settings.py
@@ -10,23 +10,34 @@ systemResWidth = x.width
 # screen_height = int(7 * 110)
 
 screen_width = 960
-screen_height = 1020
+screen_height = 920
 
 #define colours
 white = (255, 255, 255)
-
+acc = 0  # to increase the distance through time  between pipes
 # Game entites
 precision = 20
 pipe_gap = 40 + (precision * 4) # 40 -> bird height + (precision * 4 constant)
-scroll_speed = 4
-pipe_frequency = 8000 / scroll_speed #milliseconds // frequency at which the pipe occurs
+scroll_speed = 5
+pipe_frequency = (8000 + acc )/ scroll_speed #milliseconds // frequency at which the pipe occurs
 speed = 10
 pipeHeightIncrement = speed * 4 * 2
+repetations = 7
 
 # angle = 90
 angleup  = 90
 angledown = 90
 pipe_height, flag = 0, 1 # 1 means its going up and 0 means downwards
-pipeUpperLimit = angledown *4  #this goes down
-pipeLowerLimit = -(angleup * 4) #this goes up
+pipeUpperLimit = -(angleup * 4) #this goes up
+pipeLowerLimit = angledown * 4 #this goes down
 pipeHeightPiviot = -pipe_height
+
+
+"""
+?   bugga - params:  height(will be between range of motion), betweeen the center of increased distanced pipes
+?       should be able to control distance between pipe during bagga
+
+? to do - bird lives 
+?       - after death should restart before previous 2 pipe states
+
+"""
